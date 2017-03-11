92556842584cd12d31c5b3558aae8486be54af63
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-03-11 20:46:18 +0100

    zoo turned package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='zoo',
+      version='0.1',
+      description='A distributed microbial database',
+      url='none',
+      author='av',
+      author_email='none',
+      license='MIT',
+      packages=['zoo'],
+      zip_safe=False)
