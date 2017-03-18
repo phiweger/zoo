267c3e8b4be82ae0b1d8aeb7f0f40b092d30451e
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-03-18 14:05:44 +0100

    SeqDoc wip __repr__

--- zoo/objects.py
+++ zoo/objects.py
@@ -1,3 +1,7 @@
+import functools
+import json
+
+
 class SeqDoc:
     '''
     - stackoverflow, 5290715, this implementation mixes some answers
@@ -12,19 +16,118 @@ class SeqDoc:
     db = client["zoo"]
     collection = db.get_collection('influenza_a_virus')
 
-    sd = SeqDoc(**collection.find_one())
-    sd.metadata.country
+    doc = SeqDoc(**collection.find_one())
+    doc.metadata.country
     # 'Neverland'
     '''
     def __init__(self, **hashmap):
         '''
         Recursive initialization from a dict.
+
+        Example:
+
+        d = {'a': {'b': [1, 2, 3], 'c': {
+            'd': 4}, 'd': [{'hello': 'A'}, {'hello': 'B'}]}, 'b': {}}
+        doc = SeqDoc(**d)
         '''
         for key, value in hashmap.items():
-            if isinstance(value, dict):
+            print(key, value, bool(value))
+            if isinstance(value, dict) and value != {}:
+                # second condition because otherwise json serialization
+                # self.json() breaks w/ empty dicts being initialized
+                # to SeqDoc object
                 self.__dict__[key] = SeqDoc(**value)
             else:
                 self.__dict__[key] = value
+
+    def dget_attr(self, attr):  # d .. document
+        '''
+        modelled after example: stackoverflow, 31174295, much reduced
+
+        Example:
+
+        doc.dget_attr('a.d')  # same as doc.a.d
+        # [{'hello': 'A'}, {'hello': 'B'}]
+        [i['hello'] for i in doc.dget_attr('a.d')]
+        # ['A', 'B']
+        '''
+        return functools.reduce(getattr, [self] + attr.split('.'))
+
+    def dset_attr(self, attr, val):
+        '''
+        doc.dget_attr('a.c.d')
+        # 4
+        doc.dset_attr('a.c.d', 5)
+        doc.dget_attr('a.c.d')
+        # 5
+        '''
+        pre, _, post = attr.rpartition('.')
+        # 'a.c.d'.rpartition('.') == ('a.c', '.', 'd')
+        # .split() does not work for json depth > 2
+        return setattr(self.dget_attr(pre) if pre else self, post, val)
+
+    def __repr__(self):
+        '''
+        json.dumps(d, indent=True)
+        # return self._id, [i['name'] for i in self.annotation]
+
+        id, names of annotations, seq start, available derivatives, ...
+        minhash (k, n tuples)
+        msa ids
+        '''
+        if isinstance(self, SeqDoc) and not isinstance(self, dict):
+            return self.__repr__()
+        else:
+            return self.__dict__
+
+
+
+        try:
+            # w/ fields of base schema
+            pass
+        except AttributeError:
+            # pretty print complete record in json (=default?)
+            # use self.to_json()
+            pass
+
+    def __str__():
+        '''
+        try print seq, else fall back to __repr__
+        '''
+        pass
+
+    def to_json():
+        '''
+        We need not implement from_json(), because this (via a hashmap) is
+        already done to initialize a SeqDoc object.
+
+        # stackoverflow, 3768895, 10252010
+        '''
+        pass
+
+    def to_fasta():
+        pass
+
+    def get_attr():
+        '''
+        sd.get_attr('a.b.c')  # gets attribute
+        sd.get_attr('a.b.c', 'name')  # w/ annotations
+
+        d = {'a': {'b': [1, 2, 3], 'c': {
+            'd': 4}, 'd': [{'hello': 'A'}, {'hello': 'B'}]}}
+        sd = SeqDoc(**d)
+        sd.a.d
+        # [{'hello': 'A'}, {'hello': 'B'}]
+        [i['hello'] for i in sd.a.d]
+        # ['A', 'B']
+
+        # stackoverflow, 31174295
+        rgetattr(sd, 'a.d')
+        rsetattr(sd, 'a.b', [2,3,4])
+        [i['hello'] for i in rgetattr(sd, 'a.d')]
+        '''
+        pass
+
 
     def to_fasta():
         '''
@@ -33,12 +136,20 @@ class SeqDoc:
 
         Maybe use a json mask of some sort, would be elegant, i.e.
         pass a nested json with desired keys and done.
+
+        d = {'a': {'b': [1, 2, 3], 'c': {'d', 4}}}
+        sd = SeqDoc(**d)
+        # do sth. like getattr(sd, 'a.b') - this does not work
+        "mask_json"
+        ideal:
+        header=['metadata.country', 'somefixedlabel', metadata.geo.long]
+        getattr(getattr(sd, 'a'), 'b')
         '''
         pass
 
     def mask_seq():
         '''
-        apply an annotation
+        apply an annotation or user defined interval(s)
         '''
         pass
 
