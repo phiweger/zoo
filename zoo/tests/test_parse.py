1bd8351e2b99dae7e650690f648a8d28dc08cc0a
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-03-24 11:52:25 +0100

    del comment schema annotation, not allowed in JSON syntax

--- zoo/tests/test_parse.py
+++ zoo/tests/test_parse.py
@@ -1,6 +1,14 @@
-from zoo.parse import parse_date
+from zoo.parse import parse_date, location_tostr
+from Bio.SeqFeature import FeatureLocation, AfterPosition
 
 
 def test_parse_date():
     date = '1976/1/09'
     assert parse_date(date)['y'] == 1976
+
+
+def test_location_tostr():
+    f1 = FeatureLocation(5, 10, strand=-1)
+    f2 = FeatureLocation(20, AfterPosition(30), strand=0)
+    combined = f1 + f2
+    assert location_tostr(combined) == ['[5:10](-)', '[20:>30](?)']
