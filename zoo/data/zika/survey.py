546fdc0856f393f155243e222b5954952b50d870
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-03-24 17:01:23 +0100

    zika test data (survey): mh signature added

--- zoo/data/zika/survey.py
+++ zoo/data/zika/survey.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 import json
 from pyfaidx import Fasta
 from pymongo import MongoClient
+from sourmash_lib import Estimators, signature
 from uuid import uuid4
 import zoo
 
@@ -42,3 +43,31 @@ with open('survey.fa', 'w+') as outfile:
 with open('PRJNA344504.json', 'w+') as outjson:
     outjson.write(dumps(db.survey.find()))
 # fasta is 350 kb, JSON 359
+
+
+# now create a minhash of the entire sequences to search
+e16 = Estimators(ksize=16, n=1000)
+e31 = Estimators(ksize=31, n=1000)
+fn = 'survey.sig'
+
+cursor = db.survey.find({}, {'sequence': 1, '_id': 0})
+for record in cursor:
+    for k, v in record.items():
+        e16.add_sequence(v, force=True)
+        e31.add_sequence(v, force=True)
+        # force bc/ ValueError: invalid DNA character in sequence: Y
+s16 = signature.SourmashSignature(
+    email='',
+    estimator=e16,
+    name='survey',
+    filename=fn
+    )
+s31 = signature.SourmashSignature(
+    email='',
+    estimator=e31,
+    name='survey',
+    filename=fn
+    )
+
+with open(fn, 'w+') as outsig:
+    signature.save_signatures([s16, s31], fp=outsig)
