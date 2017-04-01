5a2e93b4382314ab6cb8dabe807b9986c72e6cc9
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-04-01 21:06:43 +0200

    zoo sbt_index

--- zoo/cli/minhash.py
+++ zoo/cli/minhash.py
@@ -1,12 +1,22 @@
 import click
+from pymongo import MongoClient
 from sourmash_lib import Estimators
 from sourmash_lib.sbt import SBT, GraphFactory
 from sourmash_lib.sbtmh import SigLeaf
 from sourmash_lib.signature import SourmashSignature
 
 
+@click.option('--client', default='localhost:27017')
+@click.option('--db', required=True)
+@click.option('--cell', required=True, help='Cell name.')
+@click.option(
+    '--query', required=False, help='A JSON formatted MongoDB query.')
+@click.option('--ksize', default=31)
+@click.option('--nsketch', default=1000)
+@click.option('--key', default='_id')
+@click.argument('file', type=click.Path())
 @click.command()
-def sbt_index():
+def sbt_index(client, db, cell, ksize, nsketch, key, out, file):
     '''Create a sequence Bloom tree from a cell/ database cursor.
     1. select seqs for tree
     2. assign common id (field derivative.minhash.sbt.ids)
@@ -15,8 +25,22 @@ def sbt_index():
 
     --index {raw, minhash}
     input: all of cell or cursor
+
+    TODO: add query
     '''
-    pass
+    # init SBT
+    factory = GraphFactory(ksize=ksize, starting_size=1e5, n_tables=4)
+    # 4 .. nt?
+    tree = SBT(factory, d=2)  # d .. see "n-ary " in notebook
+
+    c = MongoClient(client)[db][cell]
+    for d in c.find():
+        e = Estimators(ksize=ksize, n=nsketch)
+        e.add_sequence(d['sequence'], force=True)
+        s = SourmashSignature(estimator=e, name=d[key])
+        leaf = SigLeaf(metadata=key, data=s)
+        tree.add_node(node=leaf)
+    tree.save(file)
 
 
 @click.command()
