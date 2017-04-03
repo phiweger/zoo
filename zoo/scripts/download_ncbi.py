927140b74b068bdd5d77cc42378d01f58bc211a3
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-04-03 12:09:56 +0200

    download genbank files from ncbi script added

---
+++ zoo/scripts/download_ncbi.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python3
+# https://www.biostars.org/p/66921/
+
+import argparse
+import sys
+import os
+
+import Bio.Entrez
+
+
+RETMAX = 10**9
+GB_EXT = ".gb"
+
+
+def parse_args(arg_lst):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, required=True,
+                        help="A file with accessions to download")
+    parser.add_argument("-d", "--database", type=str, required=True,
+                        help="NCBI database ID")
+    parser.add_argument("-e", "--email", type=str, required=False,
+                        default="some_email@somedomain.com",
+                        help="An e-mail address")
+    parser.add_argument("-b", "--batch", type=int, required=False, default=100,
+                        help="The number of accessions to process per request")
+    parser.add_argument("-o", "--output_dir", type=str, required=True,
+                        help="The directory to write downloaded files to")
+
+    return parser.parse_args(arg_lst)
+
+
+def read_accessions(fp):
+    with open(fp) as acc_lines:
+        return [line.strip() for line in acc_lines]
+
+
+def accessions_to_gb(accessions, db, batchsize, retmax):
+    def batch(sequence, size):
+        l = len(accessions)
+        for start in range(0, l, size):
+            yield sequence[start:min(start + size, l)]
+
+    def extract_records(records_handle):
+        buffer = []
+        for line in records_handle:
+            if line.startswith("LOCUS") and buffer:
+                # yield accession number and record
+                yield buffer[0].split()[1], "".join(buffer)
+                buffer = [line]
+            else:
+                buffer.append(line)
+        yield buffer[0].split()[1], "".join(buffer)
+
+    def process_batch(accessions_batch):
+        # get GI for query accessions
+        query = " ".join(accessions_batch)
+        query_handle = Bio.Entrez.esearch(db=db, term=query, retmax=retmax)
+        gi_list = Bio.Entrez.read(query_handle)['IdList']
+
+        # get GB files
+        search_handle = Bio.Entrez.epost(db=db, id=",".join(gi_list))
+        search_results = Bio.Entrez.read(search_handle)
+        webenv, query_key = search_results["WebEnv"], search_results["QueryKey"]
+        records_handle = Bio.Entrez.efetch(db=db, rettype="gb", retmax=batchsize,
+                                           webenv=webenv, query_key=query_key)
+        yield from extract_records(records_handle)
+
+    accession_batches = batch(accessions, batchsize)
+    for acc_batch in accession_batches:
+        yield from process_batch(acc_batch)
+
+
+def write_record(dir, accession, record):
+    with open(os.path.join(dir, accession + GB_EXT), "w") as output:
+        print(record, file=output)
+
+
+def main(argv):
+    args = parse_args(argv)
+    accessions = read_accessions(os.path.abspath(args.input))
+    op_dir = os.path.abspath(args.output_dir)
+    if not os.path.exists(op_dir):
+        os.makedirs(op_dir)
+    dbase = args.database
+    Bio.Entrez.email = args.email
+    batchsize = args.batch
+
+    for acc, record in accessions_to_gb(accessions, dbase, batchsize, RETMAX):
+        write_record(op_dir, acc, record)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
