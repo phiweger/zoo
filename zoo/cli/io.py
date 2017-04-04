4e3c3fc0bda4310579e40a29f324c9b6a356f0de
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-04-04 11:59:45 +0200

    zoo load v0.1

--- zoo/cli/io.py
+++ zoo/cli/io.py
@@ -1,15 +1,71 @@
+import Bio.Entrez
 import click
+from progressbar import ProgressBar, UnknownLength
+import os
+from zoo.load import write_record, accessions_to_gb, read_accessions
 
 
+@click.option(
+    '--out', required=True,
+    help='The directory to write downloaded files to.')
+@click.option(
+    '--batch', required=False, default=100,
+    help='The number of accessions to process per request.')
+@click.option(
+    '--email', required=False, default='',
+    help='An e-mail address.')
+@click.option(
+    '--db', required=False, default='nucleotide',
+    help='"NCBI database ID.')
+@click.argument('file', type=click.Path())
 @click.command()
-def load():
-    '''
-    Example:
+def load(file, out, batch, email, db):
+    '''Download genbank entries from NCBI and optionally dump to JSON
+
+    This function borrows heavily from https://www.biostars.org/p/66921/.
+
+    Help:
+
+    file .. A file with accessions to download.
+
+    Desired syntax:
 
     \b
     zoo load --ncbi accession.txt result.json
+
+    Example:
+
+    zoo load --out virometest6 --email '' rna_virome_shi2016.txt
+
+    \b
+    Loading data from NCBI.
+    Batch size: 100
+    Starting download:
+    \ 10 Elapsed Time: 0:00:00
+    Done.
     '''
-    print('Load.')
+    print('Loading data from NCBI.')
+
+    RETMAX = 10**9
+
+    accessions = read_accessions(file)
+    op_dir = out
+    if not os.path.exists(op_dir):
+        os.makedirs(op_dir)
+    dbase = db
+    Bio.Entrez.email = email
+    batchsize = batch
+
+    print('Batch size:', batchsize)
+    print('Starting download:')
+    bar = ProgressBar(max_value=UnknownLength)
+    counter = 0
+    for acc, record in accessions_to_gb(
+            accessions, dbase, batchsize, RETMAX):
+        counter += 1
+        write_record(op_dir, acc, record)
+        bar.update(counter)
+    print('\nDone.')
 
 
 @click.command()
---
+++ zoo/load.py
@@ -0,0 +1,55 @@
+import Bio.Entrez
+import os
+
+
+def read_accessions(fp):
+    with open(fp) as acc_lines:
+        return [line.strip() for line in acc_lines]
+
+
+def accessions_to_gb(accessions, db, batchsize, retmax):
+    def batch(sequence, size):
+        l = len(accessions)
+        for start in range(0, l, size):
+            yield sequence[start:min(start + size, l)]
+
+    def extract_records(records_handle):
+        buffer = []
+        for line in records_handle:
+            if line.startswith("LOCUS") and buffer:
+                # yield accession number and record
+                yield buffer[0].split()[1], "".join(buffer)
+                buffer = [line]
+            else:
+                buffer.append(line)
+        yield buffer[0].split()[1], "".join(buffer)
+
+    def process_batch(accessions_batch):
+        # get GI for query accessions
+        query = " ".join(accessions_batch)
+        query_handle = Bio.Entrez.esearch(db=db, term=query, retmax=retmax)
+        gi_list = Bio.Entrez.read(query_handle)['IdList']
+
+        # get GB files
+        search_handle = Bio.Entrez.epost(db=db, id=",".join(gi_list))
+        search_results = Bio.Entrez.read(search_handle)
+        webenv = search_results["WebEnv"]
+        query_key = search_results["QueryKey"]
+
+        records_handle = Bio.Entrez.efetch(
+            db=db, rettype="gb", retmax=batchsize,
+            webenv=webenv, query_key=query_key)
+        yield from extract_records(records_handle)
+        # yield from, stackoverflow, 9708902
+
+    accession_batches = batch(accessions, batchsize)
+    for acc_batch in accession_batches:
+        yield from process_batch(acc_batch)
+
+
+GB_EXT = ".gb"
+
+
+def write_record(dir, accession, record):
+    with open(os.path.join(dir, accession + GB_EXT), "w") as output:
+        print(record, file=output)
