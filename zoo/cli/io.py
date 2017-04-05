6f7ae0eff15c4468579db1a4ff771d5db87b533d
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-04-05 10:28:30 +0200

    zoo load extendible and works for ncbi

--- zoo/cli/io.py
+++ zoo/cli/io.py
@@ -1,13 +1,12 @@
 from Bio import Entrez
 import click
 from progressbar import ProgressBar, UnknownLength
-import os
 from zoo.load import write_record, accessions_to_fmt, read_accessions
 
 
-@click.option(
-    '--out', required=True,
-    help='The directory to write downloaded files to.')
+# @click.option(
+#     '--out', required=True,
+#     help='The directory to write downloaded files to.')
 @click.option(
     '--batch', required=False, default=100,
     help='The number of accessions to process per request.')
@@ -19,10 +18,16 @@ from zoo.load import write_record, accessions_to_fmt, read_accessions
     help='"NCBI database ID.')
 @click.option(
     '--fmt', required=False, default='genbank',
-    help='"Output format: genbank | fasta | json')
-@click.argument('file', type=click.Path())
+    help='Output format: genbank | fasta | json')
+@click.option(
+    '--source', required=False, default='ncbi',
+    help='The data source.')
+@click.option(
+    '--ids', required=True,
+    help='A list of IDs, e.g. GenBank accession IDs.')
+@click.argument('out', type=click.Path())
 @click.command()
-def load(file, out, batch, email, db, fmt):
+def load(ids, out, batch, email, db, fmt, source):
     '''Download genbank entries from NCBI and optionally dump to JSON
 
     This function borrows heavily from https://www.biostars.org/p/66921/,
@@ -30,17 +35,13 @@ def load(file, out, batch, email, db, fmt):
 
     Help:
 
-    file .. A file with accessions to download.
-
-    Desired syntax:
-
-    \b
-    zoo load --ncbi accession.txt result.json
+    out .. A file name (json) or directory (genbank, fasta).
 
     Example:
 
-    zoo load --fmt json --out test.json --email '' \
-    data/rna_virome_shi2016/rna_virome_shi2016.txt
+    zoo load --source ncbi --fmt json --email '' \
+    --ids data/rna_virome_shi2016/rna_virome_shi2016.txt \
+    result.json
 
     \b
     Loading data from NCBI.
@@ -55,41 +56,44 @@ def load(file, out, batch, email, db, fmt):
     If this is the case, get a coffee, and simply try again.
 
     \b
-    Loading data from NCBI.
-    Batch size: 100
-    Starting download:
-    / 200 Elapsed Time: 0:00:08
+        Loading data from NCBI.
+        Batch size: 100
+        Starting download:
+        / 200 Elapsed Time: 0:00:08
 
     \b
-    <urlopen error [Errno 60] Operation timed out>
-    Reconnect, attempt 1/10.
-    / 600 Elapsed Time: 0:02:29
+        <urlopen error [Errno 60] Operation timed out>
+        Reconnect, attempt 1/10.
+        / 600 Elapsed Time: 0:02:29
 
     \b
-    <urlopen error [Errno 54] Connection reset by peer>
-    Reconnect, attempt 1/10.
-    - 2109 Elapsed Time: 0:07:06
-    Done.
+        <urlopen error [Errno 54] Connection reset by peer>
+        Reconnect, attempt 1/10.
+        - 2109 Elapsed Time: 0:07:06
+        Done.
+
+    For JSON as output format, data from each record is appended to the
+    specified "out" file. If it exists already, data will get appended,
+    modifying the existing file, which might be not what you want.
     '''
-    print('Loading data from NCBI.')
+    if source == 'ncbi':
+        print('Loading data from NCBI.')
+        RETMAX = 10**9
+        accessions = read_accessions(ids)
+        dbase = db
+        Entrez.email = email
+        batchsize = batch
 
-    RETMAX = 10**9
-
-    accessions = read_accessions(file)
-    dbase = db
-    Entrez.email = email
-    batchsize = batch
-
-    print('Batch size:', batchsize)
-    print('Starting download:')
-    bar = ProgressBar(max_value=UnknownLength)
-    counter = 0
-    for acc, record in accessions_to_fmt(
-            accessions, dbase, batchsize, RETMAX, fmt):
-        counter += 1
-        write_record(out, acc, record, fmt)
-        bar.update(counter)
-    print('\nDone.')
+        print('Batch size:', batchsize)
+        print('Starting download:')
+        bar = ProgressBar(max_value=UnknownLength)
+        counter = 0
+        for acc, record in accessions_to_fmt(
+                accessions, dbase, batchsize, RETMAX, fmt):
+            counter += 1
+            write_record(out, acc, record, fmt)
+            bar.update(counter)
+        print('\nDone.')
 
 
 @click.command()
