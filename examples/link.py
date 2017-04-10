a605fa5a62a523a515d002c2ec58753620b68599
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-04-10 15:45:01 +0200

    example/link.py

--- examples/link.py
+++ examples/link.py
@@ -1,16 +1,20 @@
 '''2 link examples.'''
 
 
+from Bio import Entrez, SeqIO
+from copy import deepcopy
 import json
+from pymongo import MongoClient
 from zoo import get_data
 from zoo.link import Link
+from zoo.utils import deep_get
 
 
 '''
 1.
 
-We are looking at a document that contains data on the plant pest "plum
-pox virus", that amongst other things infects peaches. In the "relative"
+We are looking at a document that contains data on the plant pest 'plum
+pox virus', that amongst other things infects peaches. In the 'relative'
 section of the document there is a link to the chloroplast sequence of the
 peach reference genome. This sequence would have been too large to embed
 in the document. However, the link allows random access to the indicated
@@ -19,14 +23,14 @@ fasta file, which is still fast.
 Our document looks like this:
 
 {
-    "_id": "someuuid",
-    "seq": "AAAATATAAA..."
+    '_id': 'someuuid',
+    'seq': 'AAAATATAAA...'
     },
-    "relative": {
-        "link": {
-            "target": "plum/plum.fa",
-            "key": "NC_014697.1",
-            "txt": "chloroplast prunus persica"
+    'relative': {
+        'link': {
+            'target': 'plum/plum.fa',
+            'key': 'NC_014697.1',
+            'txt': 'chloroplast prunus persica'
         }
     }
 }
@@ -41,7 +45,7 @@ l.is_valid()  # check if link has correct schema
 
 # get first 10 bases of chloroplast sequence
 seq = l.access_fasta(func=lambda x: x.split(' ')[0])[:10]
-# "func" is what gets passed to pyfaidx.Fasta as "key_function"
+# 'func' is what gets passed to pyfaidx.Fasta as 'key_function'
 assert seq == 'TGGGCGAACG'
 
 
@@ -51,164 +55,131 @@ assert seq == 'TGGGCGAACG'
 Now something a bit more difficult: We'll import a seqmented virus and retieve
 all segments. One way to achieve this is through a linked list, i.e. each
 segment points to the next one.
-'''
 
-'''
 How to represent a segmented virus in the document database? This notebook
 is part tutorial, part reference.
-'''
 
-
-from Bio import Entrez, SeqIO
-import json
-from pymongo import MongoClient
-from uuid import uuid4
-'''
-In other words, only after generating 1 billion UUIDs every second for the next
-100 years, the probability of creating just one duplicate would be about 50%
-(stackoverflow, 1155008).
-'''
-
-
-# http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc110
-Entrez.email = ''  # better to provide none than a random one
-
-
-'''
 [article](http://www.pnas.org/content/111/18/6744.full):
 
 Qin, X.-C. et al. A tick-borne segmented RNA virus contains genome segments
 derived from unsegmented viral ancestors. PNAS 111, 6744–6749 (2014).
 '''
-segments = ["KJ001579", "KJ001580", "KJ001581", "KJ001582"]
+segments = ['KJ001579', 'KJ001580', 'KJ001581', 'KJ001582']
 
 
 l = []
+Entrez.email = ''
 for s in segments:
     handle = Entrez.efetch(
-        db="nucleotide",
+        db='nucleotide',
         id=s,  # list of GenBank identifiers
-        rettype="fasta",  # rettype="gb",
-        retmode="text"
+        rettype='fasta',  # rettype='gb',
+        retmode='text'
         )
-    l.append(SeqIO.read(handle, "fasta"))  # "genbank"
-
-
-# m = []
-# for s in segments:
-#     handle = Entrez.efetch(
-#         db="nucleotide",
-#         id=s,  # list of GenBank identifiers
-#         rettype="gb",  # rettype="gb",
-#         retmode="text"
-#         )
-#     m.append(SeqIO.read(handle, "genbank"))
-
-
+    l.append(SeqIO.read(handle, 'fasta'))
 handle.close()
 
 
-# stackoverflow, 23333123
-s = m[1]
-for feature in s.features:
-    if feature.type == "CDS":
-        print(feature.location.extract(s).seq[:20])
+with open('jmtv.fa', 'w+') as output_handle:
+    SeqIO.write(l, output_handle, 'fasta')
+# This file can be found in zoo/data/.
 
 
-# http://biopython.org/wiki/SeqIO
-with open("jmtv.fa", "w") as output_handle:
-    SeqIO.write(l, output_handle, "fasta")
+# document schema
+schema = json.loads('''
+    {
+        "_id": "",
+        "seq": "",
+        "rel": {
+            "link": null
+        }
+    }
+    ''')
 
 
-schema = '''
-{
-    "_id": "",
-    "sequence": "",
-    "link": ""
-}
-'''
-
-
-d = json.loads(schema)
-
-
-client = MongoClient("localhost:27017")
-db = client["jmtv"]
-
-
-# populate db
-link = ""
+c = MongoClient('localhost:27017')['test']['jmtv']
+key = ''
 for i in l:
-    d = json.loads(schema)
-
-    d["_id"] = str(uuid4())[:8]
-    d["sequence"] = str(i.seq)[:20]
-    d["link"] = link
-
-    db.jmtv.insert_one(d)
-    link = d["_id"]
-
-
-collection = db.get_collection('jmtv')
-collection.find_one()
-
-
-a = db.jmtv.aggregate([
-    {"$match": {"_id": "87847a5b"}},
-    {"$graphLookup":
-        {
-            "from": "jmtv",
-            "startWith": "$link",
-            "connectFromField": "link",
-            "connectToField": "_id",
-            # "maxDepth": 2,  # defaults to all, first step is 0
-            "as": "segments"
-        }}
-    ])
+    rec = deepcopy(schema)
+    rec['_id'] = i.name
+    rec['seq'] = str(i.seq)[:10]  # for brevity, we'll use only 10 bases
+    rec['rel']['link'] = []
+    rec['rel']['link'].append(
+        Link(target='test.jmtv._id', key=key).__dict__)
+    '''
+    We don't want to pass the object itself, but the dict representation.
+    This is more convenient, when sending records around, bc/ dicts are
+    trivial to translate to JSON.
+    '''
+    if Link(**rec['rel']['link'][0]).is_valid():  # ugly
+        c.insert_one(rec)
+    key = i.name  # pass the previous segments ID as link key to the next seq
 
 
-next(a)['segments']
-'''
-[{'_id': '67d0c7a0', 'link': '', 'sequence': 'GTTAAAAGAGGCCGCCCTTT'},
- {'_id': '88b696ca', 'link': '67d0c7a0', 'sequence': 'GTTTAAAAAGCGGACCGTGC'},
- {'_id': '8ac49d0b', 'link': '88b696ca', 'sequence': 'GTTAAAAAGCGCCAGCTGAG'}]
-'''
-
-collection.update_one(
-    {'_id': '67d0c7a0'},
-    {'$set': {'link': '87847a5b'}}
+# Create a circular link, i.e. linked list becomes a circle.
+c.update_one(
+    {'_id': 'KJ001579.1'},
+    {'$set': {'rel.link': [{'key': 'KJ001582.1', 'target': 'test.jmtv._id'}]}}
     )
 
-'''
-Create a circular linking.
 
-In [118]: [print(i) for i in a]
-{'link': '87847a5b', 'sequence': 'GTTAAAAGAGGCCGCCCTTT', '_id': '67d0c7a0'}
-{'link': '67d0c7a0', 'sequence': 'GTTTAAAAAGCGGACCGTGC', '_id': '88b696ca'}
-{'link': '88b696ca', 'sequence': 'GTTAAAAAGCGCCAGCTGAG', '_id': '8ac49d0b'}
-{'link': '8ac49d0b', 'sequence': 'GTTTAAAAACGGCCGCCCTG', '_id': '87847a5b'}
+c.find_one()
 '''
-a = db.jmtv.aggregate([
-    {"$match": {"_id": "87847a5b"}},
-    {"$graphLookup":
+{'_id': 'KJ001579.1',
+ 'rel': {'link': [{'key': 'KJ001582.1', 'target': 'test.jmtv._id'}]},
+ 'seq': 'GTTAAAAGAG'}
+'''
+c.count()
+# 4
+
+
+'''
+We'll now do two things:
+
+1.1 use MongoDB's "$graphLookup" operator to travers the "graph" (linked list)
+1.2 access a record linked from another record (zoo functionality)
+'''
+
+'''
+1.1 Perform a graph traversal to get aggregate all linked elements.
+'''
+
+a = c.aggregate([
+    {'$match': {'_id': 'KJ001582.1'}},
+    {'$graphLookup':
         {
-            "from": "jmtv",
-            "startWith": "$link",
-            "connectFromField": "link",
-            "connectToField": "_id",
-            # "maxDepth": 2,  # defaults to all, first step is 0
-            "as": "segments"
+            'from': 'jmtv',
+            'startWith': '$rel.link.key',
+            'connectFromField': 'rel.link.key',
+            'connectToField': '_id',
+            'maxDepth': 100,  # defaults to all, first step is 0
+            'as': 'segments'
         }}
     ])
-next(a)['segments']
+
+
 '''
-[{'_id': '87847a5b', 'link': '8ac49d0b', 'sequence': 'GTTTAAAAACGGCCGCCCTG'},
- {'_id': '67d0c7a0', 'link': '87847a5b', 'sequence': 'GTTAAAAGAGGCCGCCCTTT'},
- {'_id': '88b696ca', 'link': '67d0c7a0', 'sequence': 'GTTTAAAAAGCGGACCGTGC'},
- {'_id': '8ac49d0b', 'link': '88b696ca', 'sequence': 'GTTAAAAAGCGCCAGCTGAG'}]
+print([i['_id'] for i in next(a)['segments']])
+# ['KJ001580.1', 'KJ001579.1', 'KJ001581.1']
 
-So it doesn't loop through graph forever, good.
+Note how even though we allow a maximum recursion depth of 100, the graph
+traversal stops after all segments are recovered.
 '''
 
+'''
+1.2 Access individually linked document.
+'''
 
-
+rec = next(c.find({'_id': 'KJ001580.1'}))
+'''
+link
+# key: KJ001579.1
+# target: test.jmtv._id
+'''
+link = Link(**deep_get(rec, 'rel.link')[0])
+next(link.access_document())
+'''
+{'_id': 'KJ001579.1',
+ 'rel': {'link': [{'key': 'KJ001582.1', 'target': 'test.jmtv._id'}]},
+ 'seq': 'GTTAAAAGAG'}
+'''
