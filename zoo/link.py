68a37305de7044648270ad1b500ce3a2889b4372
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-04-10 12:09:26 +0200

    links as objects w/ extensive tests

--- zoo/link.py
+++ zoo/link.py
@@ -1,7 +1,11 @@
+import json
+from jsonschema import validate
 from pyfaidx import Fasta
+from pymongo import MongoClient
+from zoo import get_schema
 
 
-def link_access(link, _id, func=None):
+def link_fasta(link, func=None):
     '''Given an identifier (e.g. UUID), access seq in supplement fasta file.
 
     func .. a key function to pass to pyfaidx.Fasta(), e.g.
@@ -10,14 +14,58 @@ def link_access(link, _id, func=None):
     The access is random, i.e. quick, given that an index exists. If no index
     is found (file.fai) one is created.
     '''
-    try:
-        d = link['link']
-        if d['type'] == 'filepath':
-            fa = Fasta(d['fp'], key_function=func)
-            return str(fa[_id])
-        else:
-            print('This type of link currently not supported.')
-            return
-    except KeyError:
-        print('No link schema provided as input.')
-        return
+    fa = Fasta(link.target, key_function=func)
+    return str(fa[link.key])
+
+
+def link_fastq():
+    '''TODO'''
+    pass
+
+
+class Link():
+    '''A link object.
+
+    d = {'k': 'ebola.makona._id', 'v': 'a0b5d956-a940-427d-b5ff-f3a22e750389'}
+    l = Link(d['k'], d['v'])
+    if l.is_valid():
+        print('Link is valid.')
+    deep_get(next(l.access_document()), 'meta.geo.cou')
+    'sierra_leone'
+
+    l = Link(get_data('plum/NC_001445.fa'), 'NC_001445.1')
+    l.access_fasta(func=lambda x: x.split(' ')[0])[:10]
+    # 'AAAATATAAA'
+    '''
+    def __init__(self, target, key):
+        '''
+        supported target types: field ("internal" link), fasta
+        '''
+        self.target = target
+        self.key = key
+
+    def is_valid(self, schema=None):
+        '''Validate link against a link schema.'''
+        if not schema:
+            with open(get_schema('fragments/link.json'), 'r+') as file:
+                schema = json.load(file)
+        assert validate(self.__dict__, schema=schema) is None
+        return True
+
+    def __repr__(self):
+        gen = ('{}: {}\n'.format(k, v) for k, v in self.__dict__.items())
+        out = ''
+        for i in gen:
+            out += i
+        return out
+
+    def access_document(self, client='localhost:27017'):
+        u = self.target.split('.')
+        db, cell = u[0], u[1]
+        field = '.'.join(u[2:])
+        c = MongoClient(client)[db][cell]
+        return c.find({field: self.key})  # returns > 1 docs if key not unique
+
+    def access_fasta(self, func=None):
+        return link_fasta(self, func)
+
---
+++ zoo/tests/test_link.py
@@ -0,0 +1,61 @@
+from pymongo import MongoClient
+from uuid import uuid4
+from zoo import get_data
+from zoo.link import Link
+from zoo.utils import deep_get
+
+
+def test_link_document():
+    c = MongoClient('localhost:27017')['test']['cell']
+    _id = str(uuid4())
+    c.insert_one({
+        '_id': _id,
+        'nested_key': {'key': 'brown'},
+        'nested_value': {'value': 'fox'}
+        })
+    d = {'k': 'test.cell._id', 'v': _id}  # recover _id
+    l = Link(d['k'], d['v'])
+    assert l.is_valid()
+    assert deep_get(next(l.access_document()), 'nested_value.value') == 'fox'
+
+
+def test_link_document_nested_key():
+    c = MongoClient('localhost:27017')['test']['cell']
+    _id = str(uuid4())
+    nested_key = str(uuid4())
+    c.insert_one({
+        '_id': _id,
+        'nested_key': {'key': nested_key},
+        'nested_value': {'value': 'fox'}
+        })
+    d = {'k': 'test.cell.nested_key.key', 'v': nested_key}
+    l = Link(d['k'], d['v'])
+    assert l.is_valid()
+    assert deep_get(next(l.access_document()), 'nested_value.value') == 'fox'
+
+
+def test_link_document_multiple_key_matches():
+    c = MongoClient('localhost:27017')['test']['cell']
+    nested_key = str(uuid4())
+    c.insert_one({
+        '_id': str(uuid4()),  # ID1
+        'nested_key': {'key': nested_key},  # same nested key
+        'nested_value': {'value': 'fox'}
+        })
+    c.insert_one({
+        '_id': str(uuid4()),  # ID2
+        'nested_key': {'key': nested_key},
+        'nested_value': {'value': 'jumps'}
+        })
+    d = {'k': 'test.cell.nested_key.key', 'v': nested_key}
+    l = Link(d['k'], d['v'])
+    s = set()
+    for i in l.access_document():
+        s.update([deep_get(i, 'nested_value.value')])
+    assert set(['fox', 'jumps']) == s
+
+
+def test_link_fasta():
+    l = Link(get_data('plum/NC_001445.fa'), 'NC_001445.1')
+    seq = l.access_fasta(func=lambda x: x.split(' ')[0])[:10]
+    assert seq == 'AAAATATAAA'
