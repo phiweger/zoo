e585abcd98d5698e7dadfca815c23db5fc198253
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-04-19 09:16:45 +0200

    js scripts cleaned w/ linter

--- zoo/scripts/jsondiff.js
+++ zoo/scripts/jsondiff.js
@@ -1,83 +1,69 @@
-// zoo drop --db test --cell a --force
-// zoo add --db test --cell a cell_a.json
-// node ~/repos/zoo/zoo/scripts/jsondiff.js --client localhost:27017 --db test --cell a --file cell_b.json > diff.json
+// script called by "zoo diff ..."
 
+var argv = require('minimist')(process.argv.slice(2))
+var jsondiffpatch = require('jsondiffpatch')
+var fs = require('fs')
+var MongoClient = require('mongodb').MongoClient
 
-// also check out:
-// https://github.com/mafintosh/mongojs
-// http://mongoosejs.com/
-// http://mongoosejs.com/docs/index.html
+var c
+MongoClient.connect(
+  'mongodb://' + argv.client + '/' + argv.db,
+  // localhost == 127.0.0.1
+  // 'mongodb://localhost:27017/test'
+  function (err, db) {
+    if (err) {
+      throw err
+    }
+    // console.log('connected to the mongoDB !');
+    c = db.collection(argv.cell)
+    // console.log(c.namespace)  // test.a
+    // db.close();
+    fs.readFileSync(argv.file).toString().split('\n').forEach(
+      function (line) { // err, line
+        //    console.error(err); // throw err?
+        // process.exit()
+        var l = JSON.parse(line)
+        // console.log(l._id);
 
+        // new syntax for deprecated findOne, stackoverflow, 10551313
+        c.find({'_id': l._id}).limit(1).next(function (err, doc) {
+          if (err) {
+            throw err
+          }
+          // console.log(doc);
+          // if(doc == undefined) {
+          //     console.log('sldfosdb');
+          // }
 
-// http://doduck.com/node-js-mongodb-hello-world-example/
-var argv = require('minimist')(process.argv.slice(2));
-var jsondiffpatch = require('jsondiffpatch');
-var fs = require('fs');
-var MongoClient = require('mongodb').MongoClient;
+          // null .. _id was not found in database
+          if (doc != null) {
+            var delta = jsondiffpatch.diff(doc, l)
+            // undefined .. no diff returned by jsondiffpatch
+            if (delta !== undefined) {
+              delta._id = l._id
+              // console.log(delta)
+              var result = JSON.stringify(delta)
+              console.log(result)
+            }
+          // console.log(doc)
+          } else {
+            // leaving the block empty is equivalent to Python's
+            // "pass", stackoverflow, 33383840
+          }
+          // fs.appendFileSync('./commit_id.json', result + '\n')
 
-
-var c;
-var db = MongoClient.connect(
-    'mongodb://' + argv.client + '/' + argv.db, 
-    // localhost == 127.0.0.1
-    // 'mongodb://localhost:27017/test'
-    function(err, db) {
-        if(err)
-            throw err;
-        // console.log('connected to the mongoDB !');
-        c = db.collection(argv.cell);
-        // console.log(c.namespace)  // test.a
-        // db.close();
-        fs.readFileSync(argv.file).toString().split('\n').forEach(
-            function (line) { // err, line
-            //    console.error(err); // throw err?
-            // process.exit()
-            var l = JSON.parse(line);
-            // console.log(l._id);
-
-            // new syntax for deprecated findOne, stackoverflow, 10551313
-            c.find({'_id': l._id}).limit(1).next(function(err, doc) {
-
-                if(err) {
-                    throw err;
-                }
-                // console.log(doc);
-                // if(doc == undefined) {
-                //     console.log('sldfosdb');
-                // }
-
-                // null .. _id was not found in database
-                if (doc != null) {
-                    var delta = jsondiffpatch.diff(doc, l);
-                    
-                    // undefined .. no diff returned by jsondiffpatch
-                    if (delta != undefined) {
-                        delta._id = l._id
-                        // console.log(delta)
-                        result = JSON.stringify(delta)
-                        console.log(result)
-                    }
-                    // console.log(doc)
-                } else {
-                    // leaving the block empty is equivalent to Python's
-                    // "pass", stackoverflow, 33383840
-                }
-
-                
-                // fs.appendFileSync('./commit_id.json', result + '\n');
-                
-
-                // We can pipe stdout to other processes, e.g. head. Problem
-                // is they close the connection before our program is done
-                // writing its output, which causes it to throw a fatal error
-                // (EPIPE), see explanation here: stackoverflow, 12329816
-                // To circumvent this, we catch the error like so:
-                process.stdout.on('error', function(err) {
-                    if (err.code == "EPIPE") {
-                        process.exit(0);
-                    }
-                });
-            });
-        });
-        db.close()
-    });
+          // We can pipe stdout to other processes, e.g. head. Problem
+          // is they close the connection before our program is done
+          // writing its output, which causes it to throw a fatal error
+          // (EPIPE), see explanation here: stackoverflow, 12329816
+          // To circumvent this, we catch the error like so:
+          process.stdout.on('error', function (err) {
+            if (err.code === 'EPIPE') {
+              process.exit(0)
+            }
+          })
+        })
+      })
+    db.close()
+  }
+)
