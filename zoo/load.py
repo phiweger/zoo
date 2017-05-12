cd4f805ac767479d43a64ee78207e1d5eab04ca1
Adrian Viehweger <adrian.viehweger@googlemail.com>
2017-05-12 10:19:15 +0200

    fix seq sequence discordance

--- zoo/load.py
+++ zoo/load.py
@@ -96,6 +96,9 @@ def write_record(out, accession, record, fmt):
         with open(os.path.join(out, accession + ext[fmt]), 'w') as output:
             print(record, file=output)
     elif fmt == 'json':
+        record['seq'] = dictionary.pop('sequence')
+        # Biopython calls sequence field "sequence" while we want to work
+        # with "seq". Inconvenient.
         with open(out, 'a+') as output:
             print(record, file=output)
         # 'a+' append to file
